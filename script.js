a149d14abffac13c9daa9b2a30929d1ba6411b9c
prernagulati0 <67996489+prernagulati0@users.noreply.github.com>
2021-07-04 21:34:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -30,15 +30,15 @@ async function start() {
 `}
 function displayCountries(data) {
     document.querySelector('.grid').innerHTML = `
-    ${data.map(function (image) {
+    ${data.map(function (image, index) {
         
         return `
         <div class='flex'>
-            <div id='row1'>
+            <div id='row1' >
                 <img src=${image.flag} style='object-fit:contain'>
             </div >
             <div id='row2'>
-                <h3 id='countryName' onclick='myData(this.value)'> ${image.name} </h3>
+                <h3 id='countryName' onclick='myData(${index})'> ${image.name} </h3>
                 <p>Population : ${image.population} </p>
                 <p class='countryRegion'> Region : ${image.region} </p>
                 <p> Capital : ${image.capital} </p>
@@ -59,21 +59,25 @@ function displayCountries(data) {
    
 }
 start()
-function myData(detail) {
+async function myData(detail) {
+
+    const response = await fetch('https://restcountries.eu/rest/v2/all');
+    var data = await response.json();
+    console.log(data);
+
+    console.log(typeof (detail));
     document.querySelector('.search').innerHTML = `
             <button class="back"><a href="index.html"> <i> &#8592; </i>  Back </a></button>
             `
     document.querySelector('.grid').style.display = 'none';
     document.querySelector('.data-flex').innerHTML = `
             <div class=data-col1>
-            <img src=${this.flag}>
+            <img src=${data[detail].flag}>
         </div>
         <div class=data-col2>
-            <div class='item1'>${detail.name}</div>
-            <div class='item2'>Native name : ${detail.nativeName}<br> Population : ${detail.population} <br>  Region : ${detail.region}<br> Sub Region : ${detail.subregion} <br> Capital : ${detail.capital}</div>
-            <div class='item3'>Top Level Domain : ${detail.topLevelDomain[0]} <br> Currencies :  ${detail.currencies.map(currency => currency.code)}<br> Languages : ${detail.languages.map(language => language.name)} </div>
-            <div class='item4'>Border Countries : ${detail.borders.map(border => border)}</div>
-        </div>
+            <div class='item1'>${data[detail].name}</div>
+            <div class='item2'>Native name : ${data[detail].nativeName}<br> Population : ${data[detail].population} <br>  Region : ${data[detail].region}<br> Sub Region : ${data[detail].subregion} <br> Capital : ${data[detail].capital}</div>
+            
     `
 
 }
