03fc813d4e57a5e515de24f31912d159365050ea
prernagulati0 <67996489+prernagulati0@users.noreply.github.com>
2021-07-04 22:28:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,33 +5,23 @@ let nameB;
 async function start() {
     const response = await fetch('https://restcountries.eu/rest/v2/all');
     var data = await response.json();
-    console.log(data);
-
     displayCountries(data);
-
     const html = (data.map(regionList => {
         return ` ${regionList.region}`
     }))
-    console.log(html);
     //const newhtml = jQuery.unique(html);
     var outputArray = Array.from(new Set(html))
     outputArray.pop()
-    //console.log(newhtml);
-    console.log(outputArray);
-    
     document.querySelector('.dropdown').innerHTML = `
         <option > Filter by Region </option>
         ${outputArray.map(function (x) {
-        return `<option >${x}</option>`
-            
+        return `<option >${x}</option>`    
     })}
-    
-
 `}
+
 function displayCountries(data) {
     document.querySelector('.grid').innerHTML = `
-    ${data.map(function (image, index) {
-        
+    ${data.map(function (image, index) { 
         return `
         <div class='flex'>
             <div id='row1' >
@@ -44,27 +34,15 @@ function displayCountries(data) {
                 <p> Capital : ${image.capital} </p>
             </div>
         </div>
-      `
-      
-        
-    }).join('')
-    
-    
+      `  
+    }).join('')    
         }
-`/*
-    document.querySelector('#countryName').addEventListener('click', () => {
-    myData(image);
-})*/
-    
-   
+`  
 }
 start()
 async function myData(detail) {
-
     const response = await fetch('https://restcountries.eu/rest/v2/all');
     var data = await response.json();
-    console.log(data);
-
     console.log(typeof (detail));
     document.querySelector('.search').innerHTML = `
             <button class="back"><a href="index.html"> <i> &#8592; </i>  Back </a></button>
@@ -76,20 +54,13 @@ async function myData(detail) {
         </div>
         <div class=data-col2>
             <div class='item1'>${data[detail].name}</div>
-            <div class='item2'>Native name : ${data[detail].nativeName}<br> Population : ${data[detail].population} <br>  Region : ${data[detail].region}<br> Sub Region : ${data[detail].subregion} <br> Capital : ${data[detail].capital}</div>
-            
+            <div class='item2'>Native name : ${data[detail].nativeName}<br> Population : ${data[detail].population} <br>  Region : ${data[detail].region}<br> Sub Region : ${data[detail].subregion} <br> Capital : ${data[detail].capital}</div>          
     `
-
 }
-
-
-
     document.querySelector('.darkMode').addEventListener('click', darkMode);
     function darkMode() {
         document.body.classList.toggle('light');
     }
-
-
     function regionFilter(filter) {
         const countryRegion = document.querySelectorAll('.countryRegion');
         console.log(filter);
@@ -104,20 +75,16 @@ async function myData(detail) {
                 else {
                     namep.parentElement.parentElement.style.display = 'none';
                 }  
-            
+           
         }
-        
+       
     )
     }
         
-
-    document.querySelector('.search-bar').addEventListener('input', search);
+document.querySelector('.search-bar').addEventListener('input', search);
 function search(e) {
     const val = e.target.value;
     const countryName = document.querySelectorAll('#countryName');
-    console.log(val);
-    
-
     countryName.forEach(name => {
         console.log(name.innerText);
 
@@ -126,6 +93,5 @@ function search(e) {
         } else {
             name.parentElement.parentElement.style.display = 'none';
         }   
-    })
-    
+    })   
 }
